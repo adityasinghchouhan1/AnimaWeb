20da91dd9e05bde839f802f46f41c1f0f4e3dfdf
aditya <asadityasingh2572000@gmail.com>
2025-04-21 18:40:03 +0530

    chnage in Button Component

--- Client/src/Reuse/Button.jsx
+++ Client/src/Reuse/Button.jsx
@@ -14,7 +14,7 @@ const Button = ({ Onclick, color, title }) => {
       <button
         onClick={handleClick}
         style={{ backgroundColor: color }}
-        className={`text-white font-semibold px-6 py-2 rounded-full shadow-md transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg relative overflow-hidden ${
+        className={`text-white font-semibold px-6 py-3 rounded-full shadow-md transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg relative overflow-hidden ${
           isClicked ? 'animate-pingOnce' : ''
         }`}
       >
