30717c2e5043de3373e55995402379c569278098
aditya <asadityasingh2572000@gmail.com>
2025-04-23 19:03:21 +0530

    create backend server

---
+++ Server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const server = express()
+const Bodyparser = require('body-parser')
+const core = require('cors')
+const mongoose = require('mongoose')
+
+server.use(core())
+server.use(Bodyparser.json)
+server.use(express.json)
+
+const DBConnection = mongoose.connect(
+  'mongodb+srv://adityasingh:adi7999@userdata.p1dmydp.mongodb.net/Animaweb?retryWrites=true&w=majority&appName=userdata'
+)
+
+DBConnection.then(() => {
+  server.listen(8008, (res, req) => {
+    console.log('server started')
+    console.log('db connect')
+  })
+}).catch((err) => {
+  console.log(err)
+})
